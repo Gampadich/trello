67e18537a1919d4396bdbd2278ec1ef0786a12bc
Roman Diachenko <165205043+Gampadich@users.noreply.github.com>
2025-11-27 20:36:35 +0200

    new favicon and add some code to board component

--- src/pages/board/board.tsx
+++ src/pages/board/board.tsx
@@ -1,9 +1,35 @@
-import { useState } from "react"
-import "../componentStyles/Board.css"
+import React from 'react';
+import { useState } from 'react';
+import '../componentStyles/Board.css';
 
 export const Board = () => {
-    const [title, setTitle] = useState('My test board')
-    return (
-        <header>{title}</header>
-    )
+  const [title, setTitle] = useState('Моя тестова дошка')
+  const [tables, setTable] = useState([
+    {
+      id: 1,
+      title: 'Плани',
+      cards: [
+        { id: 1, title: 'помити кота' },
+        { id: 2, title: 'приготувати суп' },
+        { id: 3, title: 'сходити в магазин' },
+      ],
+    },
+    {
+      id: 2,
+      title: 'В процесі',
+      cards: [{ id: 4, title: 'подивитися серіал' }],
+    },
+    {
+      id: 3,
+      title: 'Зроблено',
+      cards: [
+        { id: 5, title: 'зробити домашку' },
+        { id: 6, title: 'погуляти з собакой' },
+      ],
+    },
+  ]);
+  return (
+  <div className='container'>
+    <header>{title}</header>
+  </div>);
 }
