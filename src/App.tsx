8a4c154cc3e548bef90822f0e3e62e6856a4a130
Roman Diachenko <165205043+Gampadich@users.noreply.github.com>
2025-11-27 19:16:10 +0200

    add button logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Board } from './pages/board/board';
+import "./styles/App.css"
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [move, isMove] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {move 
+        ? <Link to='/board'><button onClick={() => isMove(false)}>To board page</button></Link> 
+        : <Link to='/'><button onClick={() => isMove(true)}>To home page</button></Link>}
+      <Routes>
+        <Route path='/board' element={<Board/>}/>
+        <Route path='/'/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/board/board.tsx
@@ -0,0 +1,7 @@
+import "../componentStyles/Board.css"
+
+export const Board = () => {
+    return (
+        <div>Hello This is a board page</div>
+    )
+}
