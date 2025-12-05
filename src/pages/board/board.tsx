0c85ab8d32f6b48203051cf1083297105e9261e2
Roman Diachenko <165205043+Gampadich@users.noreply.github.com>
2025-12-05 19:41:35 +0200

    not working

--- src/pages/board/board.tsx
+++ src/pages/board/board.tsx
@@ -7,17 +7,17 @@ import './Board.css';
 interface CardType {
   id: number;
   title: string;
-  color : string;
-  description : string;
+  color: string;
+  description: string;
   custom: {
-    deadline : string;
+    deadline: string;
   };
 }
 
 interface ListType {
   id: number;
-  title : string
-  cards : CardType[]
+  title: string;
+  cards: CardType[];
 }
 
 interface Responce {
@@ -26,7 +26,7 @@ interface Responce {
 }
 
 export const Board = () => {
-  const { id } = useParams<{ id : string }>();
+  const { id } = useParams<{ id: string }>();
   const [title, setTitle] = useState('');
   const [tables, setTables] = useState<ListType[]>([]);
   const [loading, setLoading] = useState(false);
@@ -36,53 +36,76 @@ export const Board = () => {
     setLoading(true);
     const fetchBoardDetails = async () => {
       try {
-        const responce : Responce = await instance.get(`/board/${id}`)
+        const responce: Responce = await instance.get(`/board/${id}`);
+        console.log('lists data:', responce.lists);
         console.log('Board details:', responce);
-        setTitle(responce.title)
-        setTables(responce.lists)
+        setTitle(responce.title);
+        setTables(responce.lists);
       } catch (error) {
         console.error('Error fetching board details:', error);
       } finally {
         setLoading(false);
       }
-    }
+    };
     fetchBoardDetails();
   }, [id]);
   if (loading) return <div>Loading...</div>;
   return (
     <div className="container">
-      <Link to='/trello'><button className='home-button'>To home page</button></Link>
-      {titleClick ? <input className='board-title-input' autoFocus={true} onBlur={(e) => {setTitle(e.target.value); isTitleClick(false)}} defaultValue={title}
-      onKeyDown={(e) => {
-        if(e.key === 'Enter'){
-          setTitle(e.currentTarget.value);
-          instance.put(`/board/${id}`, {title: e.currentTarget.value});
-          isTitleClick(false);
-        }
-      }}/> :
-      <h1 className='board-title' onClick={() => isTitleClick(true)}>{title}</h1>}
+      <Link to="/trello">
+        <button className="home-button">To home page</button>
+      </Link>
+      {titleClick ? (
+        <input
+          className="board-title-input"
+          autoFocus={true}
+          onBlur={(e) => {
+            setTitle(e.target.value);
+            isTitleClick(false);
+          }}
+          defaultValue={title}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setTitle(e.currentTarget.value);
+              instance.put(`/board/${id}`, { title: e.currentTarget.value });
+              isTitleClick(false);
+            }
+          }}
+        />
+      ) : (
+        <h1 className="board-title" onClick={() => isTitleClick(true)}>
+          {title}
+        </h1>
+      )}
       <div className="lists-wrapper">
         {tables.map((list) => (
           <CardList key={list.id} title={list.title} cards={list.cards} />
         ))}
-        {buttonClick &&
-          <input className='add-list-input' autoFocus={true} onBlur={() => isButtonClick(false)} placeholder='Put your column title...' onKeyDown={
-            async (e) => {
-              if(e.key === 'Enter'){
-                try {
-                  let newTables = {title : e.currentTarget.value, cards: []};
-                  setTables([...tables, {id: Date.now(), title: e.currentTarget.value, cards: []}]);
-                  await instance.put(`/board/${id}`, {lists : newTables});
-                  window.location.reload();
+        {buttonClick && (
+          <input
+            className="add-list-input"
+            autoFocus={true}
+            onBlur={() => isButtonClick(false)}
+            placeholder="Put your column title..."
+            onKeyDown={async (e) => {
+              if (e.key === 'Enter') {
+                const newList = {
+                  id: Date.now(),
+                  title: e.currentTarget.value,
+                  cards: [],
+                };
+                const updatedLists = [...tables, newList];
+                console.log('New List:', updatedLists);
+                setTables(updatedLists);
+                await instance.put(`/board/${id}`, { lists: updatedLists });
                 isButtonClick(false);
-                } catch (error) {
-                  console.error('Error adding new list:', error);
-                }
               }
-            }
-          }/>
-        }
-        <button className='add-list-button' onClick={() => isButtonClick(true)}>+ Add list</button>
+            }}
+          />
+        )}
+        <button className="add-list-button" onClick={() => isButtonClick(true)}>
+          + Add list
+        </button>
       </div>
     </div>
   );
